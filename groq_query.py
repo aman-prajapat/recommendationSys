57a7ddb67b4ffb21308451d5374d425e2afb6ec8
AMAN PRAJAPAT <98727830+aman-prajapat@users.noreply.github.com>
2025-11-24 17:29:59 +0530

    Update groq_query.py

--- groq_query.py
+++ groq_query.py
@@ -1,6 +1,4 @@
-# from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-load_dotenv()
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 load_dotenv()
